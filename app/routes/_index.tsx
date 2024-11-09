1f07be5ef4add58d0829c2c8b6d7aa3a915fbb7a
forwhosesakes <hayaw@Hayas-MacBook-Air.local>
2024-11-09 09:01:11 +0300

    soon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,15 @@
 
 export default function Index() {
   return (
-    <div className="flex flex-col relative  h-screen items-center justify-center">
+    <div className="flex flex-col relative  h-screen items-center justify-center bg-main">
       <div className="loader">
   <div className="hour-top"></div>
   <div className="hour-bottom"></div>
   <div className="hour-line"></div>
 </div>
       
-          
-            <img
-              src="/images/noisy-gradients.png"
-              alt="bg"
-              className="absolute blur-3xl  w-screen h-screen opacity-45"
-            />
-          <h1 className="text-6xl font-noto-sans mt-56 opacity-50"> ..قريبًا</h1>
+  
+          <h1 className="text-5xl font-noto-sans mt-56 ml-4 "> ..قريبًا</h1>
        
     
     </div>
