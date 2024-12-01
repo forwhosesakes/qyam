bc80360c2a675e2a011a42e30ed16a019e30cd71
forwhosesakes <hayaw@Hayas-MacBook-Air.local>
2024-12-02 02:53:10 +0300

    prefetch why do this to me :(

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -52,7 +52,7 @@ const Navbar = () => {
         .filter(element => canViewElement(element,(user as any)?.role ?? null))
         .map(element=>(
           <li key={element.id} className="flex cursor-pointer font-bold text-gray-700">
-            <NavLink  prefetch="viewport" to={element.link}>
+            <NavLink  to={element.link}>
               <span>{element.arabicLabel}</span>
               {element.children && element.children.length && (
                 <Icon name="below-arrow" size="sm" />
