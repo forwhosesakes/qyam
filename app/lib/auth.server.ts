49aefd825c91e4311c66d650e713236e5991cef3
forwhosesakes <hayaw@Hayas-MacBook-Air.local>
2024-12-01 06:14:16 +0300

    no shared auth instance

--- app/lib/auth.server.ts
+++ app/lib/auth.server.ts
@@ -14,63 +14,55 @@ export type Environment = {
 
 let auth: Auth | null|any = null;
 
-export const getAuth = (c: AppLoadContext) => {
-  if (auth) return auth;
-  return initAuth(c);
-};
-export const initAuth = (c: AppLoadContext): Auth => {
-  
-  auth = betterAuth({
-    
+export const getAuth = (context: AppLoadContext): Auth|any => {
+  // Create a new auth instance for each request
+  return betterAuth({
     emailAndPassword: {
       enabled: true,
       requireEmailVerification: true,
       sendResetPassword: async ({ user, url, token }, request) => {
-         sendEmail(
+        await sendEmail(
           {
             to: user.email,
             subject: "إعادة تعيين كلمة المرور",
-            text: ` قم بتعيين كلمة مرورك بالضغط على هذا : <a href="${url}"> الرابط</a>`,
-
+            text: `قم بتعيين كلمة مرورك بالضغط على هذا : <a href="${url}">الرابط</a>`,
           },
-          c.cloudflare.env.RESEND_API,
-          c.cloudflare.env.MAIN_EMAIL
+          context.cloudflare.env.RESEND_API,
+          context.cloudflare.env.MAIN_EMAIL
         );
       },
     },
     emailVerification: {
       autoSignInAfterVerification: true,
       sendOnSignUp: true,
-      
       sendVerificationEmail: async ({ user, url, token }, request) => {
-         sendEmail(
+        await sendEmail(
           {
             to: user.email,
             subject: "تأكيد التسجيل في قيم",
-            text: ` قم بتأكيد تسجيلك عن طريق دخول   <a href="${url}">  هذا الرابط</a>`,
+            text: `قم بتأكيد تسجيلك عن طريق دخول <a href="${url}">هذا الرابط</a>`,
           },
-          c.cloudflare.env.RESEND_API,
-          c.cloudflare.env.MAIN_EMAIL
+          context.cloudflare.env.RESEND_API,
+          context.cloudflare.env.MAIN_EMAIL
         );
       },
     },
-    user:{additionalFields:{
-      cvKey:{type:"string"},
-      bio:{type:"string"},
-      phone:{type:"number"},
-      acceptenceState:{type:"string"}
-    }},
+    user: {
+      additionalFields: {
+        cvKey: { type: "string" },
+        bio: { type: "string" },
+        phone: { type: "number" },
+        acceptenceState: { type: "string" }
+      }
+    },
     database: prismaAdapter(
-      client(c.cloudflare.env.DATABASE_URL),
+      client(context.cloudflare.env.DATABASE_URL),
       {
-        provider: "postgresql", //change this to your database provider
+        provider: "postgresql"
       }
     ),
     plugins: [admin()],
   });
-
-
-  return auth;
 };
 
 
