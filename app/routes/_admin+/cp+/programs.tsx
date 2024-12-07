6375792d52ae8a7da108b2b53c4aaa5ec9bb2899
gitgud16 <uchiha.axx@gmail.com>
2024-12-07 22:46:51 +0300

    programs with base64 images

--- app/routes/_admin+/cp+/programs.tsx
+++ app/routes/_admin+/cp+/programs.tsx
@@ -33,11 +33,19 @@ export async function loader({ context }: LoaderFunctionArgs) {
 export async function action({ request, context }: ActionFunctionArgs) {
   try {
     const formData = await request.formData();
+    console.log({
+      title: formData.get("title") as string,
+      link: formData.get("link") as string,
+      description: formData.get("description") as string,
+      image: formData.get("image") as string
+    });
+    
     await programDB.createProgram(
       {
         title: formData.get("title") as string,
         link: formData.get("link") as string,
         description: formData.get("description") as string,
+        image: formData.get("image") as string
       },
       context.cloudflare.env.DATABASE_URL
     );
@@ -66,12 +74,35 @@ export async function action({ request, context }: ActionFunctionArgs) {
     );
   }
 }
+
+
+async function getBase64(file: File) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
+
+
 const Programs = () => {
   const fetcher = useFetcher();
   const { programs } = useLoaderData<any>();
 
   const [title, setTitle] = useState("");
   const [link, setLink] = useState("");
+  const [selecteImage, setSelectedImage] = useState("")
+
+
+  const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      const base64String = await getBase64(file) as string;
+      setSelectedImage(base64String);
+    }
+  };
+
   return (
     <div>
       <div className="flex sm:flex-row flex-col justify-between items-center px-12 mb-8">
@@ -90,6 +121,7 @@ const Programs = () => {
               </DialogDescription>
             </DialogHeader>
             <div className="grid gap-4 py-4">
+              
               <div className="grid grid-cols-4 items-center gap-4">
                 <label htmlFor="name" className="text-right">
                   عنوان البرنامج
@@ -112,6 +144,21 @@ const Programs = () => {
                   className="col-span-3"
                 />
               </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="image" className="text-right">
+                  صورة البرنامج
+                </label>
+                <Input id="image" type="file" accept="image/*" onChange={handleImageChange} className="col-span-3" />
+              </div>
+              {selecteImage && (
+            <div className="col-span-4">
+              <img 
+                src={selecteImage} 
+                alt="Preview" 
+                className="w-32 h-32 object-cover" 
+              />
+            </div>
+          )}
             </div>
             <DialogFooter>
               <DialogClose asChild>
@@ -123,6 +170,7 @@ const Programs = () => {
                         title,
                         link,
                         description:"" ,
+                        image:selecteImage
                       },
                       { method: "POST" }
                     );
