46ab3aa00b3ced7da7993781312c735ddc36b2b2
gitgud16 <uchiha.axx@gmail.com>
2024-12-09 01:20:44 +0300

    footer fix

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,9 +24,9 @@ useEffect(()=>{
 })
   
   return (
-    <footer className="text-white bg-transparent">
-      <img className={"w-full"} src={FooterPaths} alt="footer" />
-      <div className="bg-primary py-12 md:px-24 px-8">
+    <footer className="relative mt-12 text-white bg-transparent">
+      <img className={"absolute -top-6 md:-top-28 bg-gray-50 z-10 w-full"} src={FooterPaths} alt="footer" />
+      <div className="bg-primary relative z-50 py-12 md:px-24 px-8">
         <div className="flex justify-between">
           <div>
             <img src={LogoBW} alt="logo-bw" />
